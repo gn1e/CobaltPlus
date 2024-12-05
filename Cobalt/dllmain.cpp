c319262e0f4dd4f6dd9a84a8a16f984f156175ec
nade <88284830+gn1e@users.noreply.github.com>
2024-12-05 09:04:44 -0800

    Update dllmain.cpp

--- Cobalt/dllmain.cpp
+++ Cobalt/dllmain.cpp
@@ -1,4 +1,3 @@
-
 #include <Windows.h>
 #include <iostream>
 #include <detours.h>
@@ -7,24 +6,65 @@
 #include <MinHook/MinHook.h>
 
 #define DetoursEasy(address, hook) \
-	DetourTransactionBegin(); \
+    DetourTransactionBegin(); \
     DetourUpdateThread(GetCurrentThread()); \
     DetourAttach(reinterpret_cast<void**>(&address), hook); \
     DetourTransactionCommit();
 
 void returnNone() { return; }
 
-auto FindPushWidget()
+uintptr_t FindMemoryLeakPatch()
 {
-    // OnlinePresence call
-    auto pattern = sigscan("48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 83 EC 30 48 8B E9 49 8B D9 48 8D 0D ? ? ? ? 49 8B F8 48 8B F2 E8 ? ? ? ? 4C 8B CF 48 89 5C 24 ? 4C 8B C6 48 8B D5 48 8B 48 78");
+    static uintptr_t cachedPattern = 0;
+    if (!cachedPattern) {
+        cachedPattern = sigscan("4C 8B DC 55 49 8D AB ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 49 89 5B 20 48 8B D9 4D 89 63 E0 4D 89 6B D8");
+    }
+    return cachedPattern;
+}
 
-    if (!pattern)
-        pattern = sigscan("48 8B C4 4C 89 40 18 48 89 50 10 48 89 48 08 55 53 56 57 41 54 41 55 41 56 41 57 48 8D 68 B8 48 81 EC ? ? ? ? 65 48 8B 04 25"); // 26.00+ or sum
+void Ret0Callback() {
+    return;
+}
 
-    if (!pattern)
-        pattern = sigscan("48 8B C4 48 89 58 10 48 89 70 18 48 89 78 20 55 41 56 41 57 48 8D 68 A1 48 81 EC ? ? ? ? 65 48 8B 04 25 ? ? ? ? 48 8B F9 B9 ? ? ? ? 49"); // 28.00+ or sum
+void ApplyPatch5()
+{
+    auto patch5Addr = FindMemoryLeakPatch();
 
+    if (!patch5Addr)
+    {
+        std::cout << "Failed to find the address for Patch!\n";
+        return;
+    }
+
+#ifdef USE_MINHOOK
+    if (MH_CreateHook((void*)patch5Addr, (void*)Ret0Callback, nullptr) != MH_OK)
+    {
+        std::cout << "Failed to create hook for Patch!\n";
+        return;
+    }
+    if (MH_EnableHook((void*)patch5Addr) != MH_OK)
+    {
+        std::cout << "Failed to enable hook for Patch!\n";
+        return;
+    }
+#else
+    Memcury::VEHHook::AddHook((void*)patch5Addr, Ret0Callback);
+#endif
+
+    std::cout << "Patch applied successfully.\n";
+}
+
+
+uintptr_t FindPushWidget()
+{
+    static uintptr_t pattern = 0;
+    if (!pattern) {
+        pattern = sigscan("48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 83 EC 30 48 8B E9 49 8B D9 48 8D 0D ? ? ? ?");
+        if (!pattern)
+            pattern = sigscan("48 8B C4 4C 89 40 18 48 89 50 10 48 89 48 08 55 53 56 57 41 54 41 55 41 56 41 57 48 8D 68 B8");
+        if (!pattern)
+            pattern = sigscan("48 8B C4 48 89 58 10 48 89 70 18 48 89 78 20 55 41 56 41 57 48 8D 68 A1");
+    }
     return pattern;
 }
 
@@ -38,14 +78,15 @@ void Hook(void* Target, void* Detour)
 #endif
 }
 
-bool FixMemoryLeak() // 8.51
+bool FixMemoryLeak()
 {
-    auto memoryleak = sigscan("4C 8B DC 55 57 41 56 49 8D AB ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 48 8B 01 41 B6");
-
+    static uintptr_t memoryleak = 0;
     if (!memoryleak)
     {
-        return false;
+        memoryleak = sigscan("4C 8B DC 55 57 41 56 49 8D AB ? ? ? ? 48 81 EC ? ? ? ?");
     }
+
+    if (!memoryleak) return false;
 
     Hook((void*)memoryleak, returnNone);
     return true;
@@ -100,8 +141,6 @@ bool InitializeCurlHook()
     }
     else
     {
-        // TODO find a better way to "bypass" UAC (aka switch off VEH hooks)
-
         Hook(CurlEasySetOpt, CurlEasySetOptDetour);
     }
 
@@ -183,23 +222,15 @@ DWORD WINAPI Main(LPVOID)
 {
 #ifdef SHOW_WINDOWS_CONSOLE
     AllocConsole();
-
     FILE* fptr;
     freopen_s(&fptr, "CONOUT$", "w+", stdout);
-#endif SHOW_WINDOWS_CONSOLE
-
-#ifndef URL_HOST // todo staticassert?
-    std::cout << "\n\n\n!!!!!!! URL_HOST IS NOT DEFINED !!!!!!!\n\n\n\n";
-#else
-    std::cout << "Redirecting to " << URL_PROTOCOL << "://" << URL_HOST << ":" << URL_PORT << '\n';
 #endif
 
-    std::cout << "Initializing Cobalt (made by Milxnor#3531).\n";
-
-    std::cout << "Credits:\n\n";
-
-    std::cout << "Memcury - https://github.com/kem0x/Memcury\n";
-    std::cout << "Neonite++ for most of the signatures and curl hook - https://github.com/PeQuLeaks/NeonitePP-Fixed/tree/1.4\n\n";
+#ifndef URL_HOST
+    std::cout << "URL_HOST IS NOT DEFINED!\n";
+#else
+    // wowie!
+#endif
 
 #ifdef USE_MINHOOK
     MH_Initialize();
@@ -211,37 +242,20 @@ DWORD WINAPI Main(LPVOID)
     InitializeEOSCurlHook();
     InitializeExitHook();
 
-    bool result = curlResult;
 
-    if (result)
+    if (curlResult)
     {
-        std::cout << "Cobalt v0.1 initialized sucessfully.\n";
+        ApplyPatch5();
     }
     else
     {
-        MessageBoxA(0, "Failed to initialize!", "Cobalt", MB_ICONERROR);
+        MessageBoxA(0, "Redirect | Failed", "Redirect", MB_ICONERROR);
     }
-
-#if 0
-    Sleep(3000);
-
-    if (!FixMemoryLeak())
-    {
-        std::cout << "Failed to apply memory leak fix (this may be fine)!\n";
-    }
-    else
-    {
-        std::cout << "Applied memory leak fix!\n";
-    }
-#endif
 
     return 0;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
@@ -250,7 +264,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         break;
     case DLL_PROCESS_DETACH:
         break;
-    }
+}
     return TRUE;
 }
-
