8a1500b2cf64723236aaa34cd4c72b1b35d0fc59
nade <88284830+gn1e@users.noreply.github.com>
2024-12-05 09:05:14 -0800

    Update settings.h

--- Cobalt/settings.h
+++ Cobalt/settings.h
@@ -12,7 +12,5 @@ enum class ECobaltUsage
 #define URL_PORT "3551"
 // #define USE_MINHOOK // ONLY USE IF U INJECT LATE
 
-#define SHOW_WINDOWS_CONSOLE
-
 constexpr static ECobaltUsage CobaltUsage = ECobaltUsage::Private;
 constexpr bool bIsS13Epic = false; // S13 Hybrid Windows -> IOS fake
